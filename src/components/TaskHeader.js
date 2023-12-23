41cedc7cb6dca19b963000d969845a3ad0d71d1d
Divyam Garg <dvm.garg26@gmail.com>
2023-12-23 23:53:45 +0530

    filesupdated

--- src/components/TaskHeader.js
+++ src/components/TaskHeader.js
@@ -5,7 +5,7 @@ function TaskHeader({ title, taskCount }) {
     return (
         <div className="taskHeader h2">
             <div className="left">
-                <img className="taskIcon" src="https://ik.imagekit.io/teamTaskTracker/images/Done.svg?updatedAt=1703340502424" alt="error" />
+                {/* <img className="taskIcon" src="https://ik.imagekit.io/teamTaskTracker/images/Done.svg?updatedAt=1703340502424" alt="error" /> */}
                 <span>{title + " " + taskCount}</span>
             </div>
             <div className="right">
