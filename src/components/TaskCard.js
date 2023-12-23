bc4777b057a0ccb07f9a28751245f10666474421
Divyam Garg <dvm.garg26@gmail.com>
2023-12-24 01:05:30 +0530

    files updated

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import progressImage from '../images/Progress.svg';
 
-function TaskCard({id,priority,status,title,userId,userName}) {
+function TaskCard({ id, priority, status, title, userId, userName }) {
+    const getRandomNo = () => {
+        return Math.floor(Math.random() * 256)
+    }
+    const getRandomColor = () => {
+        const randomColor = `rgba(${getRandomNo()}, ${getRandomNo()}, ${getRandomNo()}, 1)`;
+        return randomColor;
+    };
+
+    const profileIconStyle = {
+        backgroundColor: getRandomColor(),
+    };
     return (
         <div className="taskCard">
             <div className="topLine">
                 <span className="h2">{id}</span>
-                <div className="profileIcon" title={userName}>{userName.charAt(0)}
+                <div className="profileIcon" style={profileIconStyle} title={userName}>{userName.charAt(0)}
                     <div className="active"></div>
                 </div>
             </div>
