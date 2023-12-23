74035b4d8c1be22e6414f8f26e68969d6a73901e
Divyam Garg <dvm.garg26@gmail.com>
2023-12-24 00:06:31 +0530

    files updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 import useFiltrationBasedTaskUpdation from '../utils/useFiltrationBasedTaskUpdation';
 
-const Header = ({setFilteredData}) => {
+const Header = ({ setFilteredData }) => {
   const [grouping, setGrouping] = useState('status'); // Default grouping value
   const [sorting, setSorting] = useState('title'); // Default sorting value
   const handleGroupingChange = (event) => {
@@ -12,24 +12,26 @@ const Header = ({setFilteredData}) => {
     setSorting(event.target.value);
   };
 
-  useFiltrationBasedTaskUpdation(grouping, sorting,setFilteredData);
+  useFiltrationBasedTaskUpdation(grouping, sorting, setFilteredData);
 
   return (
     <div>
-      <div>
-        <label htmlFor="grouping">Grouping:</label>
-        <select id="grouping" value={grouping} onChange={handleGroupingChange}>
-          <option value="status">Status</option>
-          <option value="userId">User</option>
-          <option value="priority">Priority</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="sorting">Sorting:</label>
-        <select id="sorting" value={sorting} onChange={handleSortingChange}>
-          <option value="title">Title</option>
-          <option value="priority">Priority</option>
-        </select>
+      <div className='px-10 py-5 rounded-lg shadow-md flex flex-col gap-2 '>
+        <div className='flex gap-1'>
+          <label htmlFor="grouping">Grouping</label>
+          <select id="grouping" value={grouping} onChange={handleGroupingChange}>
+            <option value="status">Status</option>
+            <option value="userId">User</option>
+            <option value="priority">Priority</option>
+          </select>
+        </div>
+        <div className='flex gap-1'>
+          <label htmlFor="sorting">Sorting</label>
+          <select id="sorting" value={sorting} onChange={handleSortingChange}>
+            <option value="title">Title</option>
+            <option value="priority">Priority</option>
+          </select>
+        </div>
       </div>
 
       {/* The rest of your component */}
